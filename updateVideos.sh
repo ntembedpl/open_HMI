bf06f9f6067230c3acf7969e4f646aa33edd87d5
rtyma <tymarobert25@gmail.com>
2019-11-16 16:36:35 +0100

    Add update scripts

--- updateVideos.sh
+++ updateVideos.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+rm ./data/Screen1/eng/s0/intro.mp4
+rm ./data/Screen2/eng/s0/intro.mp4
+rm ./data/videos/pr1.mp4
+rm ./data/videos/pr2.mp4
+rm ./data/videos/pr3.mp4
+rm ./data/videos/pr4.mp4
+rm ./data/videos/pr5.mp4
+rm ./data/videos/pr6.mp4
+
 cp -r -v ./videos/intro.mp4 ./data/Screen1/eng/s0/
 cp -r -v ./videos/intro.mp4 ./data/Screen2/eng/s0/
 cp -r -v ./videos/vid.mp4 ./data/Screen1/eng/s1/
