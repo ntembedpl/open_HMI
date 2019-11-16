c3a30c21d8950fb86e34d61680a59c13a308324d
rtyma <tymarobert25@gmail.com>
2019-11-16 16:19:25 +0100

    Add update scripts

---
+++ updateScript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+path=$1
+
+if [ -z "$path" ]
+then
+	echo "Path is empty"
+else
+	cd ..
+	cp -r -v ./open_HMI $1
+fi
+
+
---
+++ updateVideos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cp -r -v ./videos/intro.mp4 ./data/Screen1/eng/s0/
+cp -r -v ./videos/intro.mp4 ./data/Screen2/eng/s0/
+cp -r -v ./videos/vid.mp4 ./data/Screen1/eng/s1/
+cp -r -v ./videos/crio.mp4 ./data/videos/
+cp -r -v ./videos/hot.mp4 ./data/videos/
+cp -r -v ./videos/pr1.mp4 ./data/videos/
+cp -r -v ./videos/pr2.mp4 ./data/videos/
+cp -r -v ./videos/pr3.mp4 ./data/videos/
+cp -r -v ./videos/pr4.mp4 ./data/videos/
+cp -r -v ./videos/pr5.mp4 ./data/videos/
+cp -r -v ./videos/pr6.mp4 ./data/videos/
+
+
